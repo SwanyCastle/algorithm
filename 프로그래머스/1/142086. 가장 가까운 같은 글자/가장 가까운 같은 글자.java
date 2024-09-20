d5da99b2e003280fed032aaa51e7918983644d0a
seunghwan kwak <49240318+SwanyCastle@users.noreply.github.com>
2024-09-21 04:58:00 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.14 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,18 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
-
-        String[] sArr = s.split("");
-
-        for (int i = 0; i < sArr.length; i++) {
-            answer[i] = -1;
-            for (int j = 0; j < i; j++) {
-                if (sArr[i].equals(sArr[j])) {
-                    answer[i] = i-j;
-                }
-            }
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            answer[i] = i-map.getOrDefault(ch,i+1);
+            map.put(ch,i);
         }
-
         return answer;
     }
 }
