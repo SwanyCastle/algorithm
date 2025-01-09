1f73fef408203567a80a7f5770e6d15477be2d6e
seunghwan kwak <49240318+SwanyCastle@users.noreply.github.com>
2025-01-09 12:47:11 +0900

    [Silver V] Title: 숫자 카드, Time: 1160 ms, Memory: 107696 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -9,13 +9,13 @@ public class Main {
         int N1 = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> M1 = new ArrayList<>();
+        int[] M1 = new int[N1];
 
         for (int i = 0; i < N1; i++) {
-            M1.add(Integer.parseInt(st.nextToken()));
+            M1[i] = Integer.parseInt(st.nextToken());
         }
 
-        Collections.sort(M1);
+        Arrays.sort(M1);
 
         int N2 = Integer.parseInt(br.readLine());
 
@@ -24,7 +24,7 @@ public class Main {
 
         for (int i = 0; i < N2; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if (Collections.binarySearch(M1, num) >= 0) {
+            if (customBinarySearch(M1, num)) {
                 sb.append(1).append(" ");
             } else {
                 sb.append(0).append(" ");
@@ -35,5 +35,23 @@ public class Main {
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    public static boolean customBinarySearch(int[] array, int target) {
+        int minIdx = 0;
+        int maxIdx = array.length - 1;
+
+        while (maxIdx >= minIdx) {
+            int currentIdx = (maxIdx + minIdx) / 2;
+            if (array[currentIdx] == target) {
+                return true;
+            } else if (array[currentIdx] > target) {
+                maxIdx = currentIdx - 1;
+            } else {
+                minIdx = currentIdx + 1;
+            }
+        }
+
+        return false;
     }
 }
