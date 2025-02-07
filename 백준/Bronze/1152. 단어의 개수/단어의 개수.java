aefec6be460332aff17fae1fec59aa6d157e9fe2
seunghwan kwak <49240318+SwanyCastle@users.noreply.github.com>
2025-02-07 11:54:31 +0900

    [Bronze II] Title: 단어의 개수, Time: 296 ms, Memory: 51848 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] words = br.readLine().split(" ");
+        int count = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (!words[i].isEmpty()) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
